d16f35af914d0a251fd5e4255a6b52c54377433f
German Arutyunov <garutyunov@tenderhub.ru>
2022-11-04 22:42:04 +0300

    copyright

--- graph_coder/data/wrapper.py
+++ graph_coder/data/wrapper.py
@@ -1,3 +1,7 @@
+"""
+Modified from https://github.com/jw9730/tokengt
+"""
+
 import torch
 import numpy as np
 import pyximport
--- graph_coder/modules/tokengt_graph_encoder_layer.py
+++ graph_coder/modules/tokengt_graph_encoder_layer.py
@@ -1,3 +1,7 @@
+"""
+Modified from https://github.com/jw9730/tokengt
+"""
+
 from typing import Callable, Optional
 
 import torch
