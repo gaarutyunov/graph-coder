b33cbfd2c1465a55eb0f6be5a755a24fa9f14184
German Arutyunov <garutyunov@tenderhub.ru>
2023-01-19 23:20:48 +0300

    license and copyright

--- graph_coder/datasets/ast.py
+++ graph_coder/datasets/ast.py
@@ -1,3 +1,17 @@
+#  Copyright 2023 German Arutyunov
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
 import ast
 import asyncio
 import datetime
@@ -7,7 +21,6 @@ import typing
 import aiofiles
 import networkx as nx
 import pandas as pandas
-import pydot
 import torch
 import transformers
 
@@ -167,7 +180,7 @@ class AstDataset(BaseDataset):
 
     async def _parse_source(
         self, file: Path
-    ) -> AsyncGenerator[dict[str, Union[pydot.Dot, int]], None]:
+    ) -> AsyncGenerator[dict[str, Union[nx.Graph, int]], None]:
         async with aiofiles.open(file, "r") as f:
             source = await f.read()
             try:
