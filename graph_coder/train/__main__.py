df292640e54bd6f6072ff78e1f17680a180e4c6f
German Arutyunov <garutyunov@tenderhub.ru>
2023-02-09 05:32:16 +0300

    max_length

--- graph_coder/train/__main__.py
+++ graph_coder/train/__main__.py
@@ -11,6 +11,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import warnings
+
 import click
 
 from typing import Optional
@@ -33,4 +35,5 @@ def main(
 
 
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore", message=".*is a private function and will be deprecated.*", category=UserWarning)
     main()
