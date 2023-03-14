fa4eb8b9744b0beb201176b28e9ce56a039a8c56
German Arutyunov <garutyunov@tenderhub.ru>
2023-03-14 12:28:51 +0400

    fix generator_pipe

--- graph_coder/runners/generator_pipe.py
+++ graph_coder/runners/generator_pipe.py
@@ -11,34 +11,14 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import Any, Mapping
 
 import torch
-from torch import nn
 
-from graph_coder.data import GraphCoderBatch
 from graph_coder.runners.generator import GraphCoderGeneratorRunner
 
 
 class GraphCoderGeneratorRunnerPipe(GraphCoderGeneratorRunner):
     """Runner for graph-coder generator model"""
-
-    def __init__(
-        self,
-        model: nn.Module,
-        eos_token_id: int,
-        vocab_size: int,
-        *args,
-        **kwargs,
-    ):
-        super().__init__(model, *args, **kwargs)
-        self.eos_token_id = eos_token_id
-        self.vocab_size = vocab_size
-
-    def predict_batch(self, batch: GraphCoderBatch, **kwargs) -> Mapping[str, Any]:
-        # TODO: split prediction into graph and source
-        return {"predictions": self.model(batch, **kwargs)}
-
     def _calc_loss(self, **kwargs: torch.Tensor) -> torch.Tensor:
         """Calculate loss for the given batch"""
         return self.forward_model(**kwargs)
