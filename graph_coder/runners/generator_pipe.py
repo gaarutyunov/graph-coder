198f1584c542cc0e191899fcfa8f1e224ac95fb1
German Arutyunov <garutyunov@tenderhub.ru>
2023-03-21 17:02:22 +0400

    fix loaders

--- graph_coder/runners/generator_pipe.py
+++ graph_coder/runners/generator_pipe.py
@@ -16,7 +16,6 @@ from typing import Dict, Iterator, Union
 
 import torch
 
-from catalyst.core import IRunner
 from catalyst.utils import set_global_seed
 from deepspeed import PipelineEngine
 from deepspeed.runtime.dataloader import RepeatingLoader
@@ -34,19 +33,9 @@ class GraphCoderGeneratorRunnerPipe(GraphCoderGeneratorRunner[PipelineEngine]):
     def _run_loader(self) -> None:
         with torch.set_grad_enabled(self.is_train_loader):
             if self.is_train_loader:
-                self.model.train_batch()
+                self.model.train_batch(data_iter=RepeatingLoader(self.loader))
             else:
-                self.model.eval_batch(data_iter=self.loader)
-
-    def on_epoch_start(self, runner: IRunner):
-        """Event handler."""
-        self.epoch_step += 1
-        self.epoch_metrics: Dict = defaultdict(None)
-        # storage for pure epoch-based metrics, like lr/momentum
-        self.epoch_metrics["_epoch_"] = {}
-
-        assert self.loaders is not None
-        set_global_seed(self.seed + max(0, self.engine.process_index) + self.epoch_step)
+                self.model.eval_batch(data_iter=RepeatingLoader(self.loader))
 
     def _setup_loaders(self) -> None:
         """Pass this to setup loader with deepspeed engine in `_setup_components`"""
@@ -67,7 +56,7 @@ class GraphCoderGeneratorRunnerPipe(GraphCoderGeneratorRunner[PipelineEngine]):
 
         for key, loader in loaders.items():
             if key == "train":
-                self.loaders[key] = train_loader
+                self.loaders[key] = train_loader.loader
                 continue
 
             sampler = DistributedSampler(
@@ -77,7 +66,6 @@ class GraphCoderGeneratorRunnerPipe(GraphCoderGeneratorRunner[PipelineEngine]):
                 shuffle=False,
             )
             # Build a loader and make it repeating.
-            pipe_dataloader = self.model.deepspeed_io(
+            self.loaders[key] = self.model.deepspeed_io(
                 loader.dataset, data_sampler=sampler
             )
-            self.loaders[key] = RepeatingLoader(pipe_dataloader)
